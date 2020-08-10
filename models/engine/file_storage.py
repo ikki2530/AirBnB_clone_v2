f8219a1d814f95bd7de30fd9da1400c0edb60c93
Diego Gomez <dagomez2530@gmail.com>
2020-08-10 23:52:02 +0000

    UPdating all() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls != None:
+        if cls is not None:
             new_dict = {}
             for key, val in FileStorage.__objects.items():
                 if isinstance(val, cls):
