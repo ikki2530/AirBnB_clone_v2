97c42b13cc7b2afe1bc25fea5ce85b05a804f080
Diego Gomez <dagomez2530@gmail.com>
2020-08-17 22:08:44 +0000

    Adding file for task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,16 +5,16 @@ if ! which nginx > /dev/null 2>&1; then
     sudo apt-get -y install nginx
 fi
 
-sudo mkdir -p data
-sudo mkdir -p data/web_static/
-sudo mkdir -p data/web_static/releases/
-sudo mkdir -p data/web_static/shared/
-sudo mkdir -p data/web_static/releases/test/
-sudo touch  data/web_static/releases/test/index.html
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch  /data/web_static/releases/test/index.html
 
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee data/web_static/releases/test/index.html
-sudo ln -sf data/web_static/releases/test/ data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "36 a\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
