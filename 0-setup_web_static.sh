bbbef8875e43c68b0b92aca0bc3df0470413a2b1
Diego Gomez <dagomez2530@gmail.com>
2020-08-17 19:42:42 +0000

    Adding file for task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+if ! which nginx > /dev/null 2>&1; then
+    echo "Nginx not installed"
+
+else
+    echo "Installed"
+fi
+
+sudo mkdir -p data
+sudo mkdir -p data/web_static/
+sudo mkdir -p data/web_static/releases/
+sudo mkdir -p data/web_static/shared/
+sudo mkdir -p data/web_static/releases/test/
+sudo mkdir -p data/web_static/releases/test/index.
+if [ ! -L data/web_static/current ]; then
+    echo "symbolic link exists"
+else
+    echo "symbolic links doesn't exists"
+fi
+# ln -s data/web_static/releases/test/ data/web_static/current 
