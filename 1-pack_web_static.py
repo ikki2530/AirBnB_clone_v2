513dfb6b04b8b3ac0445f9422a8856e61fe083bb
Diego Gomez <dagomez2530@gmail.com>
2020-08-18 00:52:38 +0000

    Adding file for task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""Create a .tgz file"""
 from fabric.api import local
-from datetime import datetime
+import datetime
+import os
+
 
 def do_pack():
+    """Create a .tgz file"""
+    try:
 
-    local("")
+        local("mkdir -p versions")
+        fecha = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        nombre = "versions/web_static_" + fecha + ".tgz"
+        local("tar -cvzf " + nombre + " web_static")
+        print("web_static packed: {} -> {}Bytes".format(
+            nombre, os.path.getsize(nombre)))
+        return nombre
+    except:
+        return None
