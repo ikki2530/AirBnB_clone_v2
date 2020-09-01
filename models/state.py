a0b70ad8b551f5fdf60ef30cb55473ebd83ec7cb
Diego Gomez <dagomez2530@gmail.com>
2020-09-01 21:16:06 +0000

    Adding models

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
 import os
 
 
