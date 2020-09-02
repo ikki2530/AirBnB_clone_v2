d71bc7412493bb11f9ca4e5fd5aacc319b551e07
Diego <dagomez2530@gmail.com>
2020-09-01 20:04:38 -0500

    Fixing errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,13 +4,13 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-sts = storage.all(State)
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_li():
     """Call the template to list states"""
+    sts = storage.all(State)
     return render_template('7-states_list.html', states=sts)
 
 
